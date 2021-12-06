494c6a0a009efb17c4ac0d64130427c57700bf4f
VincenzoRea <91210630+VincenzoRea@users.noreply.github.com>
2021-12-05 20:37:27 -0500

    Python Queue Simulation

---
+++ Queue Simulation.py
@@ -0,0 +1,10 @@
+#name queue simulation
+names=[]
+for i in range(10):
+    name = input('enter a customer name')
+    names.append(name)
+
+for i in range(10):
+    len(names)
+    print(names.pop(0))
+
